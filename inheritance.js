8f8accf3d7e7a5e2311c1978de2634b22f4fbc30
Peter Kwan <peterkwkwan@gmail.com>
2022-06-06 22:57:43 +0800

    inheritance.js

---
+++ inheritance.js
@@ -0,0 +1,44 @@
+class Person {
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+
+    describe() {
+        console.log(`I am ${this.name} and I am ${this.age}`)
+    }
+}
+
+class Programmer extends Person {
+    constructor(language, name, age){
+        super(name, age)
+        this.language = language
+    }
+    describe() {
+        console.log(`${this.name} is a programmer that knows ${this.language}`)
+    }
+
+    code() {
+        console.log(`${this.name} is coding`)
+    }
+}
+
+
+const chad = new Person('Chad', 17)
+const peter = new Programmer('Javascript', 'Peter', 22)
+const kyler = new Programmer('Golang', 'Kyler', 12)
+
+chad.describe()
+peter.describe()
+
+
+
+function developSoftware (programmers) {
+    for(let programmer of programmers) {
+        programmer.code()
+    }
+}
+
+const programmers = [peter, kyler]
+
+developSoftware(programmers)
